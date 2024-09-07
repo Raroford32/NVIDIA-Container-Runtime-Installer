0aed3ee6c1631c4b16849ccfe0985010200b44ea
Raroford32 <109440929+Raroford32@users.noreply.github.com>
2024-09-07 23:39:20 +0330

    Update build-tgi.sh

--- build-tgi.sh
+++ build-tgi.sh
@@ -1,24 +1,21 @@
-model=taide/Llama3-TAIDE-LX-8B-Chat-Alpha1
-volume=/home/$who/llm_model/hub 
-token=<YOUR_TOKEN>
+model=mlabonne/Hermes-3-Llama-3.1-70B-lorablated
+volume=/home/ubuntu/llm_model/hub 
+token=<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>
 
 docker run -d  --name test-tgi-server \
     --gpus all \
-    --shm-size 1g \
+    --shm-size 1500g \
     -e HUGGING_FACE_HUB_TOKEN=$token \
-    -e CUDA_VISIBLE_DEVICES=0 \
+    -e CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     -p 9891:80 \
     -p 9892:29500 \
     -v $volume:/data \
     ghcr.io/huggingface/text-generation-inference:2.1 \
     --cuda-memory-fraction 1 \
     --model-id $model \
-    --max-batch-size 10 \
-    --max-input-tokens 8000 \
-    --max-batch-prefill-tokens 8050 \
-    --max-total-tokens 8120 \
-    --max-concurrent-requests 500 \
+    --max-batch-size 1 \
+    --max-input-tokens 200000 \
+    --max-batch-prefill-tokens 200050 \
+    --max-total-tokens 250000 \
+    --max-concurrent-requests 1 \
     --max-best-of 1
-
-
-
